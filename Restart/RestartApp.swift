f9f6fa2f5b7b4ccc94b4fe928028d6e4fc48fb72
Murillo R. Araújo <mr_araujo@icloud.com>
2023-01-11 20:44:10 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Murillo R. Araújo on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
