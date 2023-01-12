c3d279543141642b089eb21406c95493984de356
Murillo R. Araújo <mr_araujo@icloud.com>
2023-01-12 19:43:49 -0300

    Finish Onboarding View

--- Restart/Screens/Onboarding.swift
+++ Restart/Screens/Onboarding.swift
@@ -11,11 +11,90 @@ struct Onboarding: View {
     @AppStorage("onboarding") var isOnboardingActive = true
 
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding").font(.largeTitle)
+        ZStack {
+            Color("ColorBlue").ignoresSafeArea(.all)
 
-            Button("Start") {
-                isOnboardingActive = false
+            VStack(spacing: 20) {
+                Spacer()
+
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+
+                    Text("It's not how much we give but")
+                    Text("how much love we put into giving")
+
+                }
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.white)
+                .padding(.horizontal, 10)
+
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+
+                Spacer()
+
+                ZStack {
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+
+                        Spacer()
+                    }
+
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingActive = false
+                        }
+
+                        Spacer()
+                    }
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
             }
         }
     }
